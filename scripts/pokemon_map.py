014778714375c97287712eb27d16b037c121dec4
Allie <103614422+obviouslyallie@users.noreply.github.com>
2026-02-23 16:30:44 -0500

    Create pokemon_map.py

---
+++ scripts/pokemon_map.py
@@ -0,0 +1,12 @@
+POKEMON_DEX = {
+    "gastly": 92, "haunter": 93, "gengar": 94,
+    "trapinch": 328, "vibrava": 329, "flygon": 330,
+    "cacnea": 331, "cacturne": 332,
+    "baltoy": 343, "claydol": 344,
+    "hippopotas": 449, "hippowdon": 450,
+    "yamask": 562, "cofagrigus": 563,
+    "maractus": 556, "golett": 622, "golurk": 623,
+    "dedenne": 702, "carbink": 703, "diancie": 719,
+    "shiinotic": 755, "sandygast": 769, "palossand": 770,
+    "togedemaru": 777, "mimikyu": 778, "sableye": 302
+}
