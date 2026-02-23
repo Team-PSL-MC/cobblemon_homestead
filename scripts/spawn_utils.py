6d3241993671a38a5ee1d0eb1b352908e6d95d93
Allie <103614422+obviouslyallie@users.noreply.github.com>
2026-02-23 15:52:16 -0500

    Update spawn_utils.py

--- scripts/spawn_utils.py
+++ scripts/spawn_utils.py
@@ -40,26 +40,45 @@ def get_conditions(spawn_obj):
     res = {"item": "None", "time": "Any", "season": "Any", "weather": "Clear", "location": None}
     found_item = (spawn_obj.get('key_item') or spawn_obj.get('needed_item') or spawn_obj.get('custom_absent_required_item'))
     cond = spawn_obj.get('condition', {})
+    
     if isinstance(cond, dict) and not found_item:
         found_item = cond.get('key_item') or cond.get('needed_item')
-    if found_item: res["item"] = str(found_item).split(':')[-1].replace('_', ' ').title()
+    if found_item: 
+        res["item"] = str(found_item).split(':')[-1].replace('_', ' ').title()
 
     def scan_cond(c):
         if not isinstance(c, dict): return
-        if 'times_of_day' in c: res["time"] = ", ".join([t.capitalize() for t in c['times_of_day']])
-        if 'season' in c: res["season"] = c['season'].capitalize()
-        if 'weather' in c: res["weather"] = c['weather'].split(':')[-1].capitalize()
+        if 'times_of_day' in c: 
+            res["time"] = ", ".join([t.capitalize() for t in c['times_of_day']])
+        if 'season' in c: 
+            res["season"] = c['season'].capitalize()
+        if 'weather' in c: 
+            res["weather"] = c['weather'].split(':')[-1].capitalize()
         if 'location' in c and isinstance(c['location'], dict):
             loc = c['location']
-            if 'block' in loc: res["location"] = "Near " + loc['block'].split(':')[-1].replace('_', ' ').title()
+            if 'block' in loc: 
+                res["location"] = "Near " + loc['block'].split(':')[-1].replace('_', ' ').title()
+        
         for logical_key in ['and', 'or']:
             if logical_key in c and isinstance(c[logical_key], list):
-                for sub_c in c[logical_key]: scan_cond(sub_c)
+                for sub_c in c[logical_key]: 
+                    scan_cond(sub_c)
+
     scan_cond(cond)
     return res
 
+def clean_location(spawn_obj, stats):
+    """Returns a clean location string (Biomes or Block context)."""
+    if stats.get("location"):
+        return stats["location"] 
+    
+    biomes_list = spawn_obj.get('condition', {}).get('biomes', ["Global"])
+    clean_biomes = [b.split(':')[-1].replace('_', ' ').title() for b in biomes_list]
+    return ", ".join(clean_biomes)
+
 def write_safe_md(filepath, content):
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    # Strips trailing whitespace to keep GitBook files clean
     clean_content = "\n".join([line.rstrip() for line in content.splitlines()])
     with open(filepath, 'w', encoding='utf-8', newline='\n') as f:
         f.write(clean_content + "\n")
